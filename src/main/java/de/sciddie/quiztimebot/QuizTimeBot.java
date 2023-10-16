92bead44c77ae3e0d33e00aea1ff238c89deb483
Sciddie <aichele.florian@web.de>
2023-10-16 22:13:00 +0200

    Bot setup   - Status   - Activity

---
+++ src/main/java/de/sciddie/quiztimebot/QuizTimeBot.java
@@ -0,0 +1,23 @@
+package de.sciddie.quiztimebot;
+
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
+
+public class QuizTimeBot {
+
+    private final ShardManager shardManager;
+
+    public QuizTimeBot() {
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(
+                System.getenv("API_KEY"))
+                .setStatus(OnlineStatus.ONLINE)
+                .setActivity(Activity.customStatus("Is watching you"));
+        shardManager = builder.build();
+    }
+
+    public static void main(String[] args) {
+        QuizTimeBot bot = new QuizTimeBot();
+    }
+}
